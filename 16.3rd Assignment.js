a058ea9d73721465fb367edfad3b9732913fbe28
asifurrahman01714 <asif.eee.cu@gmail.com>
2021-07-23 14:34:35 +0600

    budget calculator

--- 16.3rd Assignment.js
+++ 16.3rd Assignment.js
@@ -5,3 +5,11 @@ function kmToMeter(km) {
 }
 let meter = kmToMeter(4);
 console.log(meter)
+
+// Budget Calculator
+function budgetCalculator(watch, phone, laptop) {
+    let totalBudget = watch * 50 + phone * 100 + laptop * 500;
+    return totalBudget;
+}
+let myBudget = budgetCalculator(1, 1, 1);
+console.log(myBudget);
